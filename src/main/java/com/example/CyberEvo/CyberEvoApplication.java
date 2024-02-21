8a98489395d8051632369be8175d9afb1958edc4
vivek <shubhamcode423@gmail.com>
2024-02-21 23:54:46 +0530

    this is first commit.....

---
+++ src/main/java/com/example/CyberEvo/CyberEvoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CyberEvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberEvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberEvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CyberEvo/CyberEvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CyberEvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberEvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
